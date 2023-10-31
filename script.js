16d7fdb60d24af7886858bbad9bd4bffbbf40344
omar-mfarrag <omar.mustafa.ff@gmail.com>
2023-11-01 00:00:03 +0200

    Home page

---
+++ script.js
@@ -0,0 +1,13 @@
+var mobileMenu = document.getElementById("mobile-menu");
+var menuButton = document.getElementById('menu-button')
+var mobileMenuCounter = 0;
+function showAndHideMobileMenu(){
+    mobileMenuCounter ++;
+    if ( mobileMenuCounter % 2 === 0 ) {
+        mobileMenu.style.transform = "translateX(100%)";
+        menuButton.src = './assets/shared/icon-hamburger.svg';
+    }else{
+        mobileMenu.style.transform = "translateX(0%)";
+        menuButton.src = './assets/shared/icon-close.svg';
+    }
+}
